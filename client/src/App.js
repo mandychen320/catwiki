aae91d0f3d3578b159fe8a657903e02c45466535
5205689 <41498725+5205689@users.noreply.github.com>
2023-01-30 14:05:23 +1100

    added search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
 import React from "react";
 import logo from "./logo.svg";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("/api")
+    fetch("/api/breeds")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => {setData(data)});
   }, []);
 
+  console.log(data);
+
+  const search = (data, inputValue) => {
+    if (!data) return;
+    let cats = data.filter((breed) => breed.name.toLowerCase().includes(inputValue.toLowerCase()))
+    console.log(cats)
+  };
+
   return (
-    <div className="App">
+    <div className="background" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={data ? data.map((breed) => breed.name) : []}
+          sx={{ width: 400 }}
+          renderInput={(params) => <TextField 
+            {...params} 
+            label="Cat Breed" 
+            onKeyDown={(event) => event.key === 'Enter' ? search(data, event.target.value) : null}
+          />}
+        />
+        <button className="button">
+          Click here for Home!
+        </button>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -12,6 +12,18 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from CatWiki!" });
 });
 
+app.get("/api/breeds", (req, res) => {
+  console.log('breeds')
+  fetch(`https://api.thecatapi.com/v1/breeds`, {
+    headers: {
+      'x-api-key': process.env.API_KEY
+    }
+  }).then((data) => data.json())
+    .then((data) => {
+      console.log(data)
+      res.json(data)
+    })
+});
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
